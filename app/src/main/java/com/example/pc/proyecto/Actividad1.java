c93ec64e7102bad39eadd50348c318f38507fe4c
David Lobo <lobo2495@hotmail.com>
2018-05-27 18:56:42 -0600

    Se adiciona la opcion de tomar foto de producto

--- app/src/main/java/com/example/pc/proyecto/Actividad1.java
+++ app/src/main/java/com/example/pc/proyecto/Actividad1.java
@@ -1,14 +1,25 @@
 package com.example.pc.proyecto;
 
+import android.annotation.TargetApi;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.MediaScannerConnection;
 import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
@@ -17,6 +28,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -32,28 +44,46 @@ import com.example.pc.proyecto.entities.Usuario;
 import com.example.pc.proyecto.entities.Utilities;
 import com.example.pc.proyecto.entities.VolleySingleton;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 
+import static android.Manifest.permission.CAMERA;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+
 public class Actividad1 extends AppCompatActivity {
 
-    private int PICK_IMAGE = 1;
+    private static final String CARPETA_PRINCIPAL = "misImagenesApp/";//directorio principal
+    private static final String CARPETA_IMAGEN = "imagenes";//carpeta donde se guardan las fotos
+    private static final String DIRECTORIO_IMAGEN = CARPETA_PRINCIPAL + CARPETA_IMAGEN;//ruta carpeta de directorios
+    private String path;//almacena la ruta de la imagen
+    File fileImagen;
+    Bitmap bitmap;
+
+    private final int MIS_PERMISOS = 100;
+    private static final int COD_SELECCIONA = 10;
+    private static final int COD_FOTO = 20;
+
+
     Producto c;
-    Utilities imageAction = new Utilities();
-Boolean aux;
+    //Utilities imageAction = new Utilities();
+    Boolean aux;
     EditText nombreProducto;
     Spinner categoriaProducto;
     EditText precioProducto;
     ImageView imagen;
+    //RelativeLayout layoutRegistrar;//permisos
 
+    Button btnFoto;
     StringRequest stringRequest;
     JsonObjectRequest jsonObjectRequest;
-
     ProgressDialog progress;
 
-    protected void onCreate(Bundle savedInstanceState){ //Se crean todas las instancias que se utilizan en la actividad
+    protected void onCreate(Bundle savedInstanceState) { //Se crean todas las instancias que se utilizan en la actividad
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_actividad1);
 
@@ -62,49 +92,47 @@ Boolean aux;
         precioProducto = (EditText) findViewById(R.id.text_categoria);
         imagen = (ImageView) findViewById(R.id.imagen_agregar);
 
+
+        btnFoto = (Button) findViewById(R.id.btnFoto);
+
+        if (solicitaPermisosVersionesSuperiores()) {
+            btnFoto.setEnabled(true);
+        } else {
+            btnFoto.setEnabled(false);
+        }
+
         llenar_spinner();
 
-        Button agreg= (Button) findViewById(R.id.btn_agregar);
+        Button agreg = (Button) findViewById(R.id.btn_agregar);
         c = new Producto();
         agreg.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
 
 
-                    loadWebServiceRegister();
+                loadWebServiceRegister();
 
             }
         });
-        Button pros= (Button) findViewById(R.id.but_cancel);
-        pros.setOnClickListener(new View.OnClickListener()
-        {
-            public void onClick(View v)
-            {
+        Button pros = (Button) findViewById(R.id.but_cancel);
+        pros.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
                 nombreProducto.setText("");
                 categoriaProducto.setSelection(0);
                 precioProducto.setText("");
             }
         });
-        imagen.setOnClickListener(new View.OnClickListener()
-        {
+        btnFoto.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view)
-            {
-                Intent intent = new Intent();
-                intent.setType("image/*");
-                intent.setAction(Intent.ACTION_GET_CONTENT);
-                startActivityForResult(Intent.createChooser(intent, "Seleccione la foto.."),PICK_IMAGE);
+            public void onClick(View view) {
+                mostrarDialogOpciones();
 
             }
         });
 
 
-
     }
 
-
-
-    public void llenar_spinner() // se llena el spinner de las categorias
-    {
+    public void llenar_spinner(){
         Spinner s1;
         final String[] presidents = {
                 "Seleccione una categoria",
@@ -128,14 +156,13 @@ Boolean aux;
                 this, android.R.layout.simple_spinner_item, presidents);
 
 
-
-
         s1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view,
                                        int position, long id) {
 
             }
+
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
             }
@@ -146,55 +173,37 @@ Boolean aux;
 
     }
 
+
     private boolean Vacio() { //se validan los campos para que no vayan datos incorectos hacia la bd
 
         EditText nombreProducto = (EditText) findViewById(R.id.text_nombre);
         Spinner categoriaProducto = (Spinner) findViewById(R.id.sp_categoria);
         EditText precioProducto = (EditText) findViewById(R.id.text_categoria);
-        if(nombreProducto.length()==0 || precioProducto.length()==0)
-        {
+        if (nombreProducto.length() == 0 || precioProducto.length() == 0) {
             MensajeOK("Campos de precio o productos vacio ");
             return true;
-        }
-        else if(categoriaProducto.getSelectedItem().toString().equals("Seleccione una categoria"))
-        {
+        } else if (categoriaProducto.getSelectedItem().toString().equals("Seleccione una categoria")) {
             MensajeOK("Seleccione una categoria");
             return true;
-        }
-        else {return false;}
-    }
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {//se convierte de imagen a string para ser almacenada en la bd
-        super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == RESULT_OK && requestCode == PICK_IMAGE) {
-            if (data != null) {
-                Uri imageUri = data.getData();
-                try {
-                    ImageView imagen = (ImageView) findViewById(R.id.imagen_agregar);
-                    InputStream stream = getContentResolver().openInputStream(imageUri);
-                    Bitmap option = BitmapFactory.decodeStream(stream);
-                    imagen.setImageBitmap(option);
-
-                    c.setFoto(imageAction.getByte(option));
-
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                }
-            }
+        } else {
+            return false;
         }
     }
 
-    public void MensajeOK(String msg){
+    public void MensajeOK(String msg) {
         View v1 = getWindow().getDecorView().getRootView();
-        AlertDialog.Builder builder1 = new AlertDialog.Builder( v1.getContext());
+        AlertDialog.Builder builder1 = new AlertDialog.Builder(v1.getContext());
         builder1.setMessage(msg);
         builder1.setCancelable(true);
         builder1.setPositiveButton("OK",
                 new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {} });
+                    public void onClick(DialogInterface dialog, int id) {
+                    }
+                });
         AlertDialog alert11 = builder1.create();
         alert11.show();
-        ;}
+        ;
+    }
 
 
     private void loadWebServiceRegister() { //se envia la peticion al sefvidor para guardar el objeto producto en la bd del server
@@ -218,7 +227,7 @@ Boolean aux;
 
                                 imagen.setImageResource(R.drawable.ic_menu_gallery);
                                 Toast.makeText(getBaseContext(), "Se ha registrado con exito", Toast.LENGTH_SHORT).show();
-                                aux=true;
+                                aux = true;
                             } else {
                                 Toast.makeText(getBaseContext(), "No se ha registrado ", Toast.LENGTH_SHORT).show();
                                 Log.i("RESPUESTA: ", "" + response);
@@ -235,14 +244,13 @@ Boolean aux;
                 @Override
                 protected Map<String, String> getParams() throws AuthFailureError {
 
-                    if(c.getFoto()== null)
-                    {
+                    if (c.getFoto() == null) {
                         c.setFoto(" ");
                     }
                     String nombre = nombreProducto.getText().toString();
                     String categoria = categoriaProducto.getSelectedItem().toString();
                     String precio = precioProducto.getText().toString();
-                    String imagen = c.getFoto();
+                    String imagen = convertirImgString(bitmap);
                     String usuario = Usuario.USUARIO.getNombre();
 
                     Map<String, String> parametros = new HashMap<>();
@@ -260,15 +268,212 @@ Boolean aux;
         }
 
     }
-    public boolean onKeyDown(int keyCode, KeyEvent event) { //se modifica la accion al presionar el botton de atras del celular
+
+    private void mostrarDialogOpciones() {
+        final CharSequence[] opciones={"Tomar Foto","Elegir de Galeria","Cancelar"};
+        final AlertDialog.Builder builder=new AlertDialog.Builder(this);
+        builder.setTitle("Elige una Opción");
+        builder.setItems(opciones, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                if (opciones[i].equals("Tomar Foto")){
+                    abriCamara();
+                }else{
+                    if (opciones[i].equals("Elegir de Galeria")){
+                        Intent intent=new Intent(Intent.ACTION_PICK,
+                                MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                        intent.setType("image/");
+                        startActivityForResult(intent.createChooser(intent,"Seleccione"),COD_SELECCIONA);
+                    }else{
+                        dialogInterface.dismiss();
+                    }
+                }
+            }
+        });
+        builder.show();
+    }
+
+    private void abriCamara() {
+        File miFile=new File(Environment.getExternalStorageDirectory(),DIRECTORIO_IMAGEN);
+        boolean isCreada=miFile.exists();
+
+        if(isCreada==false){
+            isCreada=miFile.mkdirs();
+        }
+
+        if(isCreada==true){
+            Long consecutivo= System.currentTimeMillis()/1000;
+            String nombre=consecutivo.toString()+".jpg";
+
+            path=Environment.getExternalStorageDirectory()+File.separator+DIRECTORIO_IMAGEN
+                    +File.separator+nombre;//indicamos la ruta de almacenamiento
+
+            fileImagen=new File(path);
+
+            Intent intent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT,Uri.fromFile(fileImagen));
+
+            if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.N)
+            {
+                String authorities=getApplicationContext().getApplicationContext().getPackageName() + ".provider";
+                Uri imageUri= FileProvider.getUriForFile(getApplicationContext(),authorities,fileImagen);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+             }else
+            {
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(fileImagen));
+            }
+            startActivityForResult(intent,COD_FOTO);
+
+            ////
+
+        }
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode){
+            case COD_SELECCIONA:
+                Uri miPath=data.getData();
+                imagen.setImageURI(miPath);
+
+                try {
+                    bitmap=MediaStore.Images.Media.getBitmap(getContentResolver(),miPath);
+                    imagen.setImageBitmap(bitmap);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                break;
+            case COD_FOTO:
+                MediaScannerConnection.scanFile(this, new String[]{path}, null,
+                        new MediaScannerConnection.OnScanCompletedListener() {
+                            @Override
+                            public void onScanCompleted(String path, Uri uri) {
+                                Log.i("Path",""+path);
+                            }
+                        });
+
+                bitmap= BitmapFactory.decodeFile(path);
+                imagen.setImageBitmap(bitmap);
+
+                break;
+        }
+        bitmap=redimensionarImagen(bitmap,600,800);
+    }
+
+    private Bitmap redimensionarImagen(Bitmap bitmap, float anchoNuevo, float altoNuevo) {
+
+        int ancho=bitmap.getWidth();
+        int alto=bitmap.getHeight();
+
+        if(ancho>anchoNuevo || alto>altoNuevo){
+            float escalaAncho=anchoNuevo/ancho;
+            float escalaAlto= altoNuevo/alto;
+
+            Matrix matrix=new Matrix();
+            matrix.postScale(escalaAncho,escalaAlto);
+
+            return Bitmap.createBitmap(bitmap,0,0,ancho,alto,matrix,false);
+        }else{
+            return bitmap;
+        }
+    }
+
+    private boolean solicitaPermisosVersionesSuperiores() {
+        if (Build.VERSION.SDK_INT<Build.VERSION_CODES.M){//validamos si estamos en android menor a 6 para no buscar los permisos
+            return true;
+        }
+
+        //validamos si los permisos ya fueron aceptados
+        if((checkSelfPermission(WRITE_EXTERNAL_STORAGE)== PackageManager.PERMISSION_GRANTED)&&checkSelfPermission(CAMERA)==PackageManager.PERMISSION_GRANTED){
+            return true;
+        }
+
+
+        if ((shouldShowRequestPermissionRationale(WRITE_EXTERNAL_STORAGE)||(shouldShowRequestPermissionRationale(CAMERA)))){
+            cargarDialogoRecomendacion();
+        }else{
+            requestPermissions(new String[]{WRITE_EXTERNAL_STORAGE, CAMERA}, MIS_PERMISOS);
+        }
+
+        return false;//implementamos el que procesa el evento dependiendo de lo que se defina aqui
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode==MIS_PERMISOS){
+            if(grantResults.length==2 && grantResults[0]==PackageManager.PERMISSION_GRANTED && grantResults[1]==PackageManager.PERMISSION_GRANTED){//el dos representa los 2 permisos
+                Toast.makeText(this,"Permisos aceptados",Toast.LENGTH_SHORT);
+                btnFoto.setEnabled(true);
+            }
+        }else{
+            solicitarPermisosManual();
+        }
+    }
+
+
+    private void solicitarPermisosManual() {
+        final CharSequence[] opciones={"si","no"};
+        final AlertDialog.Builder alertOpciones=new AlertDialog.Builder(this);//estamos en fragment
+        alertOpciones.setTitle("¿Desea configurar los permisos de forma manual?");
+        alertOpciones.setItems(opciones, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                if (opciones[i].equals("si")){
+                    Intent intent=new Intent();
+                    intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                    Uri uri=Uri.fromParts("package",getPackageName(),null);
+                    intent.setData(uri);
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(getBaseContext(),"Los permisos no fueron aceptados",Toast.LENGTH_SHORT).show();
+                    dialogInterface.dismiss();
+                }
+            }
+        });
+        alertOpciones.show();
+    }
+
+
+    private void cargarDialogoRecomendacion() {
+        AlertDialog.Builder dialogo=new AlertDialog.Builder(this);
+        dialogo.setTitle("Permisos Desactivados");
+        dialogo.setMessage("Debe aceptar los permisos para el correcto funcionamiento de la App");
+
+        dialogo.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    requestPermissions(new String[]{WRITE_EXTERNAL_STORAGE,CAMERA},100);
+                }
+            }
+        });
+        dialogo.show();
+    }
+
+    private String convertirImgString(Bitmap bitmap) {
+
+        ByteArrayOutputStream array=new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG,100,array);
+        byte[] imagenByte=array.toByteArray();
+        String imagenString= Base64.encodeToString(imagenByte,Base64.DEFAULT);
+
+        return imagenString;
+    }
+
+    /*public boolean onKeyDown(int keyCode, KeyEvent event) { //se modifica la accion al presionar el botton de atras del celular
         // TODO Auto-generated method stub
         if (keyCode == event.KEYCODE_BACK) {
             MensajeSalir("Seguro que desea salir de la aplicación");
         }
         return super.onKeyDown(keyCode, event);
-    }
+    }*/
 
-    public void MensajeSalir(String msg) { //pop up de confirmacion para salir de esta actividad
+    /*public void MensajeSalir(String msg) { //pop up de confirmacion para salir de esta actividad
         View v1 = getWindow().getDecorView().getRootView();
         AlertDialog.Builder builder1 = new AlertDialog.Builder( v1.getContext());
         builder1.setMessage(msg);
@@ -285,5 +490,5 @@ Boolean aux;
                     } });
         AlertDialog alert11 = builder1.create();
         alert11.show();
-    }
+    }*/
 }
